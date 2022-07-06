a107b7cdc7d598d48468db38c21d13b0ee0cc892
Nojus Stasiunas <84374792+nojussta@users.noreply.github.com>
2022-07-06 14:42:43 +0300

    fix/js-code-formatting-updated

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-const daysCon = document.getElementById('days');
-const hoursCon = document.getElementById('hours');
-const minutesCon = document.getElementById('minutes');
-const secondsCon = document.getElementById('seconds');
+const daysCon = document.getElementById('days')
+const hoursCon = document.getElementById('hours')
+const minutesCon = document.getElementById('minutes')
+const secondsCon = document.getElementById('seconds')
 
-const secondYear = "27 Aug 2022";
+const secondYear = '27 Aug 2022'
 
-function countDate() {
-    const secondYearDate = new Date(secondYear);
-    const currentDate = new Date();
+function countDate () {
+  const secondYearDate = new Date(secondYear)
+  const currentDate = new Date()
 
-    const summedTime = (secondYearDate - currentDate) / 1000;
+  const summedTime = (secondYearDate - currentDate) / 1000
 
-    const days = Math.floor(summedTime / 3600 / 24);
-    const hours = Math.floor(summedTime / 3600) % 24;
-    const minutes = (Math.floor(summedTime / 60) % 60);
-    const seconds = Math.floor(summedTime % 60);
+  const days = Math.floor(summedTime / 3600 / 24)
+  const hours = Math.floor(summedTime / 3600) % 24
+  const minutes = (Math.floor(summedTime / 60) % 60)
+  const seconds = Math.floor(summedTime % 60)
 
-    console.log(days, hours, minutes, seconds);
+  console.log(days, hours, minutes, seconds)
 
-    daysCon.innerHTML = days;
-    hoursCon.innerHTML = hours;
-    minutesCon.innerHTML = timeFormat(minutes);
-    secondsCon.innerHTML = timeFormat(seconds);
+  daysCon.innerHTML = days
+  hoursCon.innerHTML = hours
+  minutesCon.innerHTML = timeFormat(minutes)
+  secondsCon.innerHTML = timeFormat(seconds)
 }
 
-function timeFormat(time){
-    return time < 10 ? (`0${time}`) : time;
+function timeFormat (time) {
+  return time < 10 ? (`0${time}`) : time
 }
 
-countDate();
+countDate()
 
-setInterval(countDate, 1000);
+setInterval(countDate, 1000)
